03e9b32d26c36e7a4aef960f215c6ac23620b501
John Spiak <jspiak@cusa.canon.com>
2022-06-16 10:55:04 -0400

    Final Commit V1

--- force-app/main/default/lwc/fM3DeviceCount/fM3DeviceCount.js
+++ force-app/main/default/lwc/fM3DeviceCount/fM3DeviceCount.js
@@ -1,6 +1,10 @@
 import { LightningElement, wire } from 'lwc';
-import fetch from '@salesforce/apex/fetchOnlineDevices.fetchDevices';
+import fetchOnline from '@salesforce/apex/fetchOnlineDevices.fetchOnline';
+import fetchOffline from '@salesforce/apex/fetchOnlineDevices.fetchOffline';
+import fetchRetired from '@salesforce/apex/fetchOnlineDevices.fetchRetired';
 
 export default class FM3DeviceCount extends LightningElement {
-    @wire(fetch) online;
+    @wire(fetchOnline) online;
+    @wire(fetchOffline) offline;
+    @wire(fetchRetired) retired;
 }
